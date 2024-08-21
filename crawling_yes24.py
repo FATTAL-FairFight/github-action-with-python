71758d497996208ab215b5034057717c2fbfdcfa
FATTAL_FairFight <103544911+FATTAL-FairFight@users.noreply.github.com>
2024-08-21 16:32:14 +0900

    Update crawling_yes24.py

--- crawling_yes24.py
+++ crawling_yes24.py
@@ -21,18 +21,19 @@ def extract_book_data(soup):
     :param soup: BeautifulSoup soup Object
     :return: contents(str)
     """
-
     upload_contents = ''
-    new_books = soup.select(".goodsTxtInfo")
+    new_books = soup.select(".goodsTxtInfo")  # 클래스 이름이나 요소 구조를 다시 확인하세요.
     url_prefix = "http://www.yes24.com"
 
     for new_book in new_books:
-        book_name = new_book.select("a")[0].text
-        url_suffix = new_book.select("a")[1].attrs['href']
+        # 새로운 HTML 구조에 맞게 요소 선택자를 수정
+        book_name = new_book.select("a")[0].text.strip()  # 책 제목을 올바르게 가져옵니다.
+        url_suffix = new_book.select("a")[0].attrs['href']  # href 속성을 정확하게 선택합니다.
         url = url_prefix + url_suffix
-        price = new_book.select(".priceB")[0].text
+        price = new_book.select(".priceB")[0].text.strip()  # 가격 정보를 올바르게 가져옵니다.
 
         content = f"<a href={url}>" + book_name + "</a>" + ", " + price + "<br/>\n"
         upload_contents += content
 
     return upload_contents
+
